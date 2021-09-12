64ad2323b3eb1d201eaa5fd3a386d53c3e6ba685
krzys <krzysztofmuller1@gmail.com>
2021-09-13 01:05:39 +0200

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* cw.1 pętle while */
+        /* cw.4  */
 
-        /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
-          Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indexu (podać nr indexu)
-          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
-        */
-
-
-        /* zad 2 - stworzyć tablicę typu int wielkosci 10, przypisac jej dowolne wartości,
-          przeiterować tablicę po elementach i wyswietlić, użyć pętli while
-        */
-
-        /* zad.3
-        a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true ----> while(true)
-        b) sprawdzić jak wyżej dla for (;true;)
+        /* zad.1 napisać 3 pętle for i użyc:
+        a) w pierwszej pętli break
+        b) w drugiej pętli continue
+        c) w trzeciej pętli return
         */
 
     }
