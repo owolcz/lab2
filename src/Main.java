8b09cb6d7564df30827dfdb8c2d1276c946e0d15
krzys <krzysztofmuller1@gmail.com>
2021-09-12 15:11:36 +0200

    ćw 2

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* przykład.2 pętle for */
+        /* ćw.2 pętle for */
 
-        /* przykładowa pętla */
-        for (int i = 0; i < 10; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println("\n");
+        /* zad.1 Napisz program który będzie iterował od 100 do 0,
+         *  i wyświetlał liczby które są podzielne przez ostatnią liczbę waszego indeksu
+         * (jesli 0 to przez 2 ostatnie) */
 
-        int i = 5;
-        for (; i > 0; ) {
-            System.out.print(i + " ");
-            i--;
-        }
 
-        /* Przykład z tablicą */
-        System.out.println("\n");
-        String[] tab = new String[]{"Ala", "ma", "kota"};
-        for (int j = 0; j < tab.length; j++) {
-            System.out.print(tab[j] + " ");
-        }
-
-        /* przykład forEach */
-        String[] tablica = new String[]{"To", "jest", "moja", "tablica"};
-        System.out.println("\n");
-        for (String zmienna : tablica) {
-            System.out.print(zmienna + " ");
-        }
-
-        /* Przykład forEach
-         * toCharArray() - zamienia napis (String) na tablicę typu char[] (tworzymy tablicę znakową ze stringa)
-         * return powoduje zakończenie metody (wyjście z funkcji) */
-        String napis = "abcdefg xyz";
-        for (char znak : napis.toCharArray()) {
-            if (znak == 'z') {
-                System.out.println("z");
-                return;
-            }
-            System.out.print(znak + "-");
-        }
+        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
+         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
+         * za pomocą pętli forEach */
 
     }
 }
