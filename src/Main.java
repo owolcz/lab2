19867200ffbd86a739f9f45cb4bd4e090746ef85
krzys <krzysztofmuller1@gmail.com>
2021-09-12 19:23:49 +0200

    przykład 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,65 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static int[] tablica;
 
     public static void main(String[] args) {
 
-        /* ćw.2 pętle for */
+        /* Przykład 3 switch case */
 
-        /* zad.1 Napisz program który będzie iterował od 100 do 0,
-         *  i wyświetlał liczby które są podzielne przez ostatnią liczbę waszego indeksu
-         * (jesli 0 to przez 2 ostatnie) */
+        /* proste użycie switch case i default */
+        int a = 2;
+        switch (a) {
+            case 2:
+                System.out.println("warunek spełniony");
+                break;
+            default:
+                System.out.println("warunek niespełniony");
+                break;
+        }
+        /* wiele case-ow */
+        Scanner scan = new Scanner((System.in));
+        System.out.print("Podaj liczbę (0 - 5): ");
+        switch (scan.nextInt()) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("Jeden");
+                break;
+            case 2:
+                System.out.println("Dwa");
+                break;
+            case 3:
+                System.out.println("Trzy");
+                break;
+            case 4:
+                System.out.println("Cztery");
+                break;
+            default:
+                System.out.println("Pięć");
+                break;
+        }
 
-
-        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
-         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
-         * za pomocą pętli forEach */
+        /* Switch case często używany jest z pętlą, gdzie case-y działają jak else if() */
+        String text = "aabaacbbaa";
+        int[] tablica = {0, 0, 0};
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'a':
+                    ++tablica[0];
+                    break;
+                case 'b':
+                    ++tablica[1];
+                    break;
+                case 'c':
+                    ++tablica[2];
+                    break;
+            }
+        }
+        System.out.printf("\nLiczba występuje\na: %d - razy\nb: %d - razy\nc: %d - razy",
+                tablica[0], tablica[1], tablica[2]);
 
     }
 }
