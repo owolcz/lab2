b3a5e45e73e19b60a6342b849a7c6504e674a04a
krzys <krzysztofmuller1@gmail.com>
2021-09-17 00:22:49 +0200

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
         /* zad.1 Napisz program który będzie iterował od 100 do 0,
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
-         * (jesli 0 to przez 2 ostatnie) */
+         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
-         * za pomocą pętli forEach */
+         * za pomocą pętli forEach powiększone o 11 */
 
     }
 }
