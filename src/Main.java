1bca76646f84d54e88fc3280d4b5b74249389fbd
krzys <krzysztofmuller1@gmail.com>
2021-09-17 00:14:22 +0200

    ćwiczenie 5

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         a) stworzyć tablicę typu int[20],
         b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 20,
         c) napisać metodę, która wypisze otrzymane wartości,
+        d) wstawić wywołanie metody do metody main
         */
 
         /* zad.2
@@ -22,10 +23,18 @@ public class Main {
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
             wyświetla w konsoli string który ma być waszym imieniem,
-        b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-ma parametrami typu String, która wyświetli
+        b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
             wasze imie i nazwisko,
         c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
             nazwisko i wiek (int), a następnie wszystko wyświetli,
+        */
+
+        /* zad.4
+        a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
+            coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
+        b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
+            którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
+        c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
     }
