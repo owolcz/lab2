42e68622c660c68d8605385d784b2f3550d22ab6
krzys <krzysztofmuller1@gmail.com>
2021-09-19 23:47:57 +0200

    ćw_6

--- src/Main.java
+++ src/Main.java
@@ -6,62 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* Przyklad_6 Listy
-           operują na typach objektowych */
-        List<Integer> lista1 = new ArrayList<>();
-        List<BigInteger> lista2 = new ArrayList<>();
-        List<String> lista3 = new ArrayList<>();
-        List<BigDecimal> lista4 = new ArrayList<>();
-        List<Character> lista5 = new ArrayList<>(); // objektowy char
-        List<Object> lista6 = new ArrayList<>();
+        /* ćw_6 Listy */
 
-        /* przypisanie wartości */
-        lista1.add(1);
-        lista1.add(5);
-        lista1.add(12);
-        lista1.add(3);
-        lista1.add(7);
-        lista1.add(9);
-        lista1.add(2);
-        System.out.println(lista1 + ", wielkość listy - " + lista1.size()); // [1, 5, 12, 3, 7, 9, 2]
-
-        /* odczytanie wartości elementu */
-        System.out.println("Pierwszy element: " + lista1.get(0) +
-                "\nDrugi element listy: " + lista1.get(1));
-
-        /* usówanie wartości, element o indeksie 2 został usunięty z listy */
-        lista1.remove(2);
-        System.out.println(lista1 + ", wielkość listy - " + lista1.size()); // [1, 5, 3, 7, 9, 2]
-
-        /* stworzenie listy z tablicy */
-        Integer[] tablica1 = new Integer[]{1, 5, 6, 8, 90, 3};
-        lista1 = Arrays.stream(tablica1).toList();
-        System.out.println(lista1); // [1, 5, 6, 8, 90, 3]
-
-        /* dynamiczna inicjalizacja listy */
-        lista1 = Arrays.asList(1, 4, 6, 9, 8, 3, 2);
-        System.out.println(lista1); // [1, 4, 6, 9, 8, 3, 2]
-
-        /* Sortowanie */
-        Collections.sort(lista1);
-        System.out.println(lista1); // [1, 2, 3, 4, 6, 8, 9]
-
-        /* Sortowanie od końca */
-        Collections.sort(lista1, Collections.reverseOrder());
-        System.out.println(lista1); // [9, 8, 6, 4, 3, 2, 1]
+        /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
+                Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
+                elementów i elementami mają być nazwy zwierząt.
+                b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
+                -- podpowiedź: for(String zmienna : List<String>)
+                c) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
+                elementów listy
+          */
 
 
-        /* Przykład tworzenia Set-a */
-        Set<String> imiona = new TreeSet<>();
-        imiona.add("Kasia");
-        imiona.add("Ania");
-        imiona.add("Ania");
-        imiona.add("Wojtek");
-        imiona.add("Zuza");
-        imiona.add("Zuza");
+         /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
+                niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
+                 a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
+         */
 
-        System.out.println("imiona: " + imiona); // [Ania, Kasia, Wojtek, Zuza]
-        System.out.println(imiona.size()); // 4
 
     }
 
