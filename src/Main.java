568a509d4c3906feb547acc4877554288d09cfda
krzys <krzysztofmuller1@gmail.com>
2021-09-19 23:29:21 +0200

    przykład_6

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -25,7 +23,15 @@ public class Main {
         lista1.add(7);
         lista1.add(9);
         lista1.add(2);
-        System.out.println(lista1); // [1, 5, 12, 3, 7, 9, 2]
+        System.out.println(lista1 + ", wielkość listy - " + lista1.size()); // [1, 5, 12, 3, 7, 9, 2]
+
+        /* odczytanie wartości elementu */
+        System.out.println("Pierwszy element: " + lista1.get(0) +
+                "\nDrugi element listy: " + lista1.get(1));
+
+        /* usówanie wartości, element o indeksie 2 został usunięty z listy */
+        lista1.remove(2);
+        System.out.println(lista1 + ", wielkość listy - " + lista1.size()); // [1, 5, 3, 7, 9, 2]
 
         /* stworzenie listy z tablicy */
         Integer[] tablica1 = new Integer[]{1, 5, 6, 8, 90, 3};
@@ -36,6 +42,26 @@ public class Main {
         lista1 = Arrays.asList(1, 4, 6, 9, 8, 3, 2);
         System.out.println(lista1); // [1, 4, 6, 9, 8, 3, 2]
 
+        /* Sortowanie */
+        Collections.sort(lista1);
+        System.out.println(lista1); // [1, 2, 3, 4, 6, 8, 9]
+
+        /* Sortowanie od końca */
+        Collections.sort(lista1, Collections.reverseOrder());
+        System.out.println(lista1); // [9, 8, 6, 4, 3, 2, 1]
+
+
+        /* Przykład tworzenia Set-a */
+        Set<String> imiona = new TreeSet<>();
+        imiona.add("Kasia");
+        imiona.add("Ania");
+        imiona.add("Ania");
+        imiona.add("Wojtek");
+        imiona.add("Zuza");
+        imiona.add("Zuza");
+
+        System.out.println("imiona: " + imiona); // [Ania, Kasia, Wojtek, Zuza]
+        System.out.println(imiona.size()); // 4
 
     }
 
