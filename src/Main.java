e5959f1cfb04e6664de4d5344a2b59e098f95b96
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2021-10-30 10:22:51 +0200

    cw 3 zad 1

--- src/Main.java
+++ src/Main.java
@@ -55,5 +55,30 @@ public class Main {
         System.out.println("\n");
         System.out.println("Zadanie 2: ");
 
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0, 0, 0, 0, 0};
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o':
+                    ++tablica[0];
+                    break;
+                case 'p':
+                    ++tablica[1];
+                    break;
+                case 'x':
+                    ++tablica[2];
+                    break;
+
+                case 'z':
+                    ++tablica[3];
+                    break;
+
+                case 'q':
+                    ++tablica[4];
+                    break;
+            }
+        }
+        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy",
+                tablica[0], tablica[1], tablica[2], tablica[3], tablica[4]);
     }
 }
