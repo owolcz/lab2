9dd1d0e8f6ccf82ef2cd97f3a39825d026e12c60
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2022-01-06 00:16:15 +0100

    cw 6

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,90 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        /* ćwiczenie 5 Metody */
+        public static void main(String[] args) {
+            /* ćwiczenie 5 Metody */
 
-        /* zad.1
-        a) stworzyć tablicę typu int[20],
-        b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 20,
-        c) napisać metodę, która wypisze otrzymane wartości,
-        d) wstawić wywołanie metody do metody main
-        */
+            /* zad.1
+            a) stworzyć tablicę typu int[20],
+            b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 20,
+            c) napisać metodę, która wypisze otrzymane wartości,
+            d) wstawić wywołanie metody do metody main
+            */
 
-//        TablicaString();
-//        metoda2(metod5());
-
-
-
-        Imie("Oskar");
-        Imie("Oskar", "Wołcz");
-        Imie("Oskar", "Wołcz", 22);
-
-        sumInt(20);
-    }
-
-    public static void metoda2(int[] tab) {
-        for (int zmienna : tab) {
-            System.out.print(zmienna + " "); // wyświetla numer liter w kodzie ASCII
-        }
-    }
+    //        TablicaString();
+    //        metoda2(metod5());
 
 
-    public static int[] metod5() {
-        int[] tab = new int[20];
-        for (int i = 19; i > 0; i--) {
-            tab[i] = i;
-        }
-        return tab;
-    }
-
-        public static void Tablicaa() {
-            int[] tablica = new int[20];
-
-            for (int i = 0; i < 20 ; i++)
-                tablica[i] = i + 1;
-
-            int co = tablica[10];
+//            System.out.println("Zadanie 2: ");
+//            TablicaString();
+//
+//
+//            System.out.println("Zadanie 3: ");
+//            Imie("Oskar");
+//            Imie("Oskar", "Wołcz");
+//            Imie("Oskar", "Wołcz", 22);
 
 
-                System.out.println("Kolejna komórka to: " + tablica[20]);
+            metod5(40, 20);
+
         }
 
-
-
-
-        public static void UzupelnijTablice(int zmienna) {
-            System.out.println("Zadanie 1: ");
-            Scanner input = new Scanner(System.in);
-            int[] numbers = new int[20];
-            for (int i = 0; i < numbers.length; i++)
-            {
-                System.out.println("Please enter number");
-                numbers[i] = input.nextInt();
+        public static int[] metod5(int zmienna1, int zmienna2) {
+            int max = 20;
+            int[] tab = new int[20];
+            for (int i = 40; i > max; i--) {
+                tab[i] = i;
+                System.out.println(tab[i]);
             }
-
-            for (int a  : numbers) {
-                System.out.println(a+ 11);
-            }
+            return tab;
         }
+
+//        public static void metoda2(int[] tab) {
+//            for (int zmienna : tab) {
+//                System.out.print(zmienna + " "); // wyświetla numer liter w kodzie ASCII
+//            }
+//        }
+//
+//
+//        public static int[] metod5() {
+//            int[] tab = new int[20];
+//            for (int i = 19; i > 0; i--) {
+//                tab[i] = i;
+//            }
+//            return tab;
+//        }
+
+//        public static void Tablicaa() {
+//            int[] tablica = new int[20];
+//
+//            for (int i = 0; i < 20 ; i++)
+//                tablica[i] = i + 1;
+//
+//            int co = tablica[10];
+//
+//
+//                System.out.println("Kolejna komórka to: " + tablica[20]);
+//        }
+//
+//
+//
+//
+//        public static void UzupelnijTablice(int zmienna) {
+//            System.out.println("Zadanie 1: ");
+//            Scanner input = new Scanner(System.in);
+//            int[] numbers = new int[20];
+//            for (int i = 0; i < numbers.length; i++)
+//            {
+//                System.out.println("Please enter number");
+//                numbers[i] = input.nextInt();
+//            }
+//
+//            for (int a  : numbers) {
+//                System.out.println(a+ 11);
+//            }
+//        }
 
         /* zad.2
         a) stworzyc tablicę typu String[6],
@@ -82,14 +97,17 @@ public class Main {
         public static void TablicaString(){
             Scanner input = new Scanner(System.in);
             String[] tekst = new String[6];
-            for (int i = 0; i < tekst.length; i++)
+
             {
-                System.out.println("Please enter text");
-                tekst[i] = String.valueOf(input.nextInt());
+                //wprowadzenie wartości
+                for(int i=0; i<5; i++)
+
+                    tekst[i] = input.nextLine();
             }
 
-
-
+            //wyświetlenie
+            for(int i=4; i>=0; i--)
+                System.out.println("element: "+ tekst[i]);
         }
 
         /* zad.3
@@ -101,17 +119,17 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
-    public static void Imie(String imie) {
-        System.out.println("a) Moje imię: " + imie);
-    }
+        public static void Imie(String imie) {
+            System.out.println("a) Moje imię: " + imie);
+        }
 
-    public static void Imie(String imie, String nazwisko) {
-        System.out.println("b) Moje imię oraz nazwisko: " + imie + " " + nazwisko);
-    }
+        public static void Imie(String imie, String nazwisko) {
+            System.out.println("b) Moje imię oraz nazwisko: " + imie + " " + nazwisko);
+        }
 
-    public static void Imie(String imie, String nazwisko, int wiek) {
-        System.out.println("c) Moje imię oraz nazwisko i wiek: " + imie + " " + nazwisko + " " + wiek);
-    }
+        public static void Imie(String imie, String nazwisko, int wiek) {
+            System.out.println("c) Moje imię oraz nazwisko i wiek: " + imie + " " + nazwisko + " " + wiek);
+        }
 
 
 
@@ -123,11 +141,10 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
-    static int sumInt(int n) {
-        if (n == 0)
-            return 0;
-        return (n % 10 + sumInt(n / 10));
-    }
+//        public static int[] metoda7(int x, int y){
+//
+//
+//        }
 }
 
 
