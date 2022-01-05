36adc19466d2de14f9b683ef0836bcd7954f9d6b
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2022-01-06 00:58:01 +0100

    cw 6

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,13 @@ public class Main {
             d) wstawić wywołanie metody do metody main
             */
 
-    //        TablicaString();
-    //        metoda2(metod5());
-
-
+            System.out.println("Zadanie 1: " );
+            int[] tablica1 = metod5(20);
+            metoda2(tablica1);
+            System.out.println("\n");
+//
+//
+//
 //            System.out.println("Zadanie 2: ");
 //            TablicaString();
 //
@@ -26,65 +29,27 @@ public class Main {
 //            Imie("Oskar", "Wołcz");
 //            Imie("Oskar", "Wołcz", 22);
 
-
-            metod5(40, 20);
+//            int[] zmiennaA = metoda6(5, 5);
+//            metod4(zmien)
 
         }
 
-        public static int[] metod5(int zmienna1, int zmienna2) {
-            int max = 20;
-            int[] tab = new int[20];
-            for (int i = 40; i > max; i--) {
-                tab[i] = i;
-                System.out.println(tab[i]);
+
+            //Zadanie 1
+            public static int[] metod5(int zmienna) {
+                int[] tab = new int[zmienna];
+                for (int i = 40; i >= 20; i--) {
+                    tab[i - 40] = i;
+                }
+                return tab;
             }
-            return tab;
-        }
 
-//        public static void metoda2(int[] tab) {
-//            for (int zmienna : tab) {
-//                System.out.print(zmienna + " "); // wyświetla numer liter w kodzie ASCII
-//            }
-//        }
-//
-//
-//        public static int[] metod5() {
-//            int[] tab = new int[20];
-//            for (int i = 19; i > 0; i--) {
-//                tab[i] = i;
-//            }
-//            return tab;
-//        }
+            public static void metoda2(int[] tab) {
+                for (int zmienna : tab) {
+                    System.out.print(zmienna + " ");
+                }
+            }
 
-//        public static void Tablicaa() {
-//            int[] tablica = new int[20];
-//
-//            for (int i = 0; i < 20 ; i++)
-//                tablica[i] = i + 1;
-//
-//            int co = tablica[10];
-//
-//
-//                System.out.println("Kolejna komórka to: " + tablica[20]);
-//        }
-//
-//
-//
-//
-//        public static void UzupelnijTablice(int zmienna) {
-//            System.out.println("Zadanie 1: ");
-//            Scanner input = new Scanner(System.in);
-//            int[] numbers = new int[20];
-//            for (int i = 0; i < numbers.length; i++)
-//            {
-//                System.out.println("Please enter number");
-//                numbers[i] = input.nextInt();
-//            }
-//
-//            for (int a  : numbers) {
-//                System.out.println(a+ 11);
-//            }
-//        }
 
         /* zad.2
         a) stworzyc tablicę typu String[6],
@@ -93,7 +58,7 @@ public class Main {
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
 
-
+        //Zadanie 2
         public static void TablicaString(){
             Scanner input = new Scanner(System.in);
             String[] tekst = new String[6];
@@ -119,6 +84,8 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
+
+        //Zadanie 3
         public static void Imie(String imie) {
             System.out.println("a) Moje imię: " + imie);
         }
@@ -141,10 +108,18 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
-//        public static int[] metoda7(int x, int y){
-//
-//
-//        }
+            public static int[] metoda6(int zmienna1, int zmienna2) {
+                int suma = zmienna1 + zmienna2;
+                int[] tab = new int[suma];
+                for (int i = 0; i < suma; i++) {
+                    tab[i] = i;
+                }
+                return tab;
+            }
+            public static int metod4(int zmienna) {
+                System.out.print("To jest metoda 4 - ");
+                return zmienna + 7;
+            }
 }
 
 
